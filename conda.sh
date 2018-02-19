68681ee79af3ccbe8afc2770fd37fdb845d75d1b
Hosszulepes <hacka95@gmail.com>
2018-02-19 21:13:14 +0100

    Conda changed.

--- conda.sh
+++ conda.sh
@@ -3,4 +3,4 @@
 wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
 chmod +x Anaconda3-5.1.0-Linux-x86_64.sh
 ./Anaconda3-5.1.0-Linux-x86_64.sh -b
-#rm Anaconda3-5.1.0-Linux-x86_64.sh -f
+rm Anaconda3-5.1.0-Linux-x86_64.sh -f
