6569ae49d5acdb9c7436b26664f0fa7b724d3f24
pocandraspoc <pocandraspoc@gmail.com>
2018-07-11 15:07:56 +0200

    conda 3

--- 104_conda.sh
+++ 104_conda.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 conda_in(){
-	wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
-	chmod +x Anaconda3-5.1.0-Linux-x86_64.sh
-	./Anaconda3-5.1.0-Linux-x86_64.sh -b
-	rm Anaconda3-5.1.0-Linux-x86_64.sh -f
+	wget https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+	chmod +x Anaconda3-5.2.0-Linux-x86_64.sh
+	./Anaconda3-5.2.0-Linux-x86_64.sh -b
+	rm Anaconda3-5.2.0-Linux-x86_64.sh -f
 	echo 'PATH=$HOME/anaconda3/bin:$PATH' >> ~/.bashrc
 	source ~/.bashrc
-	shutdown now
+	#shutdown now
 	#conda create --name insta python=3.6 pip -y
 }
 
