d3b3d2751d5c4110571d81b3ff759c833e3f0d5f
pocandraspoc <pocandraspoc@gmail.com>
2018-07-18 09:23:53 +0200

    tor-02

--- 206_tor.sh
+++ 206_tor.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-#tor
 
-wget https://www.torproject.org/dist/torbrowser/7.5.6/tor-browser-linux64-7.5.6_en-US.tar.xz
-tar -xf ~/tor/tor-browser-linux64-7.5.6_en-US.tar.xz -C ~/tor/
+tor_in(){
+	mkdir ~/tor2/
+	mkdir ~/tor/
+	wget https://www.torproject.org/dist/torbrowser/7.5.6/tor-browser-linux64-7.5.6_en-US.tar.xz -P ~/tor2/
+	
+	tar -xf ~/tor2/tor-browser-linux64-7.5.6_en-US.tar.xz -C ~/tor/
+	rm ~/tor2/tor-browser-linux64-7.5.6_en-US.tar.xz
+	rm ~/tor2 -fr
+}
+
+tor_in
